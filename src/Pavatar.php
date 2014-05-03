3e15910d6c59a01428702370750c3b770b11f5dd
Christian Archer <chrstnarchr@aol.com>
2014-05-04 02:25:19 +0400

    psr - formatting

--- src/Pavatar.php
+++ src/Pavatar.php
@@ -13,19 +13,16 @@ class Pavatar
 
     public static function Discover($url)
     {
-        if (strpos($url, '://') === false)
-        {
+        if (strpos($url, '://') === false) {
             $url = 'http://'. $url;
         }
 
         $avatar = self::getPavatarFrom($url);
 
-        if (self::$method !== self::METHOD_DIRECT)
-        {
+        if (self::$method !== self::METHOD_DIRECT) {
             $avatar = self::getDirectUrl($avatar, $exists, false);
 
-            if ($exists === false)
-            {
+            if ($exists === false) {
                 return false;
             }
         }
@@ -43,11 +40,9 @@ class Pavatar
         global $_pavatar_mime_type;
         $_url = '';
 
-        if ($url)
-        {
+        if ($url) {
             $testurl = $url;
-            do
-            {
+            do {
                 $headers = self::getHeaders($testurl);
                 $_url = @$headers['x-pavatar'];
                 self::$method = self::METHOD_HEADER;
@@ -55,35 +50,28 @@ class Pavatar
             } while (!$_url && $testurl);
         }
 
-        if (!$_url && $url)
-        {
-            if (class_exists('DOMDocument'))
-            {
+        if (!$_url && $url) {
+            if (class_exists('DOMDocument')) {
                 $dom = new DOMDocument();
-                if (@$dom->loadHTML(self::getUrlContents($url)))
-                {
+                if (@$dom->loadHTML(self::getUrlContents($url))) {
                     $links = $dom->getElementsByTagName('link');
                     $metas = $dom->getElementsByTagName('meta');
 
-                    for ($i = 0; $i < $links->length; $i++)
-                    {
+                    for ($i = 0; $i < $links->length; $i++) {
                         $rels = strtolower($links->item($i)->getAttribute('rel'));
                         $relsarr = preg_split('/\s+/', $rels);
 
-                        if (array_search('pavatar', $relsarr) !== FALSE)
-                        {
+                        if (array_search('pavatar', $relsarr) !== false) {
                             $_url = html_entity_decode($links->item($i)->getAttribute('href'));
                             $_pavatar_mime_type = $links->item($i)->getAttribute('type');
                             self::$method = self::METHOD_HTML_LINK;
                         }
                     }
 
-                    for ($i = 0; $i < $metas->length; $i++)
-                    {
+                    for ($i = 0; $i < $metas->length; $i++) {
                         $httpequiv = strtolower($metas->item($i)->getAttribute('http-equiv'));
 
-                        if ($httpequiv == 'x-pavatar')
-                        {
+                        if ($httpequiv == 'x-pavatar') {
                             $_url = html_entity_decode($metas->item($i)->getAttribute('content'));
                             self::$method = self::METHOD_HTML_META;
                         }
@@ -98,13 +86,11 @@ class Pavatar
             }
         }
 
-        if (!$_url && $url)
-        {
+        if (!$_url && $url) {
             $_url = self::getDirectUrl($url, $exists);
             self::$method = self::METHOD_DIRECT;
 
-            if (!$exists)
-            {
+            if (!$exists) {
                 $urlp = parse_url($url);
                 if (isset($urlp['port']))
                     $port = ':' . $urlp['port'];
@@ -115,8 +101,7 @@ class Pavatar
                 $_url = self::getDirectUrl($url, $exists);
             }
 
-            if (!$exists)
-            {
+            if (!$exists) {
                 $_url = false;
                 self::$method = self::METHOD_NOTFOUND;
             }
@@ -132,8 +117,7 @@ class Pavatar
         $headers = @get_headers($url);
         $headerc = count((array) $headers);
 
-        for ($i = 0; $i < $headerc; $i++)
-        {
+        for ($i = 0; $i < $headerc; $i++) {
             $ci = strpos($headers[$i], ':');
             $headn = strtolower(substr($headers[$i], 0, $ci));
             $headv = ltrim(substr($headers[$i], $ci + 1));
@@ -150,35 +134,30 @@ class Pavatar
         $in_headers = true;
         $ret = '';
 
-        do
-        {
+        do {
             $headers = self::getHeaders($url);
-            if (@$headers['location'])
-            {
+            if (@$headers['location']) {
                 $url = $headers['location'];
             }
-        } while(@$headers['location']);
+        } while (@$headers['location']);
 
         $urlp = parse_url($url);
         if (empty($urlp['port']))
             $urlp['port'] = 80;
 
-        if (!@$urlp['path'])
-        {
+        if (!@$urlp['path']) {
             $urlp['path'] = '/';
         }
 
         @$fh = fsockopen($urlp['host'], $urlp['port']);
-        if ($fh)
-        {
+        if ($fh) {
             fwrite($fh, 'GET ' . $urlp['path'] . ' HTTP/1.1' . "\r\n");
             fwrite($fh, 'Host: ' . $urlp['host'] . "\r\n");
             fwrite($fh, 'User-Agent: PHP-Pavatar' . "\r\n");
             fwrite($fh, "Connection: close\r\n");
             fwrite($fh, "\r\n");
 
-            while (!feof($fh))
-            {
+            while (!feof($fh)) {
                 if ($in_headers || !trim($ret))
                     $ret = '';
 
@@ -187,9 +166,7 @@ class Pavatar
                 if (!trim($ret))
                     $in_headers = false;
             }
-        }
-        else
-        {
+        } else {
             $_pavatar_mime_type = 'text/plain';
         }
 
@@ -200,8 +177,7 @@ class Pavatar
     private static function getDirectUrl($url, & $exists, $add_suffix = true)
     {
         $_url = $url;
-        if ($add_suffix)
-        {
+        if ($add_suffix) {
             $sep = substr($url, -1, 1) == '/' ? '' : '/';
             $_url = $url . $sep . 'pavatar.png';
         }
@@ -210,13 +186,11 @@ class Pavatar
 
         $exists = $headers && (preg_match('/[45]\\d\\d/', $headers[0]) === 0);
 
-        if (strstr($headers[0], '301') !== false)
-        {
+        if (strstr($headers[0], '301') !== false) {
             $ret = self::getHeaders($_url, false);
             return self::getDirectUrl($ret['location'], $exists, false);
         }
-        if (strstr($headers[0], '302') !== false)
-        {
+        if (strstr($headers[0], '302') !== false) {
             $ret = self::getHeaders($_url, false);
             self::getDirectUrl($ret['location'], $exists, false);
             return $_url;
